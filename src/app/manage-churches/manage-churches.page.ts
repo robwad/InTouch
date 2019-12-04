4f7f7ef2bde02c60d0b3dc793ee8393b9718dc7e
robwad <darleywaddilove@college.harvard.edu>
2019-12-03 21:44:45 -0800

    Manage Churches Done

--- src/app/manage-churches/manage-churches.page.ts
+++ src/app/manage-churches/manage-churches.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GroupCrudService } from '../services/groupcrud.service'
 import * as firebase from "firebase/app";
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-manage-churches',
@@ -10,37 +11,44 @@ import * as firebase from "firebase/app";
 export class ManageChurchesPage implements OnInit {
 	groups: any;
 	user: any;
+	display_groups: any;
 
-  constructor(private groupCrud: GroupCrudService) { }
+	constructor(private groupCrud: GroupCrudService,
+  	private firestore: AngularFirestore) { }
 
-  ngOnInit() {
-	firebase.auth().onAuthStateChanged( user => {
-  		if (user) { this.user = user }
-  	});
+  	ngOnInit() {
+  		firebase.auth().onAuthStateChanged( user => {
+	  		if (user) { this.user = user }
+	  	});
 
+	  	this.groupCrud.read_Group().subscribe(data => { 
+	  		this.groups = data.map(e => {
+	  			let uid = this.user.uid;
+	  			let uid_list = e.payload.doc.data()['user_ids']
+	  			if (!uid_list){
+	  				uid_list = {}
+	  			}
+	  			return {
+			        id: e.payload.doc.id,
+			        Name: e.payload.doc.data()['name'],
+			        Owner: e.payload.doc.data()['owner'],
+			        Subs: uid_list,
+			        Org: "dummyOrg"
+			    };
+			})
+			this.display_groups = []
+			for (let i of this.groups) {
+				if (i.Owner == this.user.uid) {
+					this.display_groups.push(i)
+				}
+			}
+		});
+	}
 
-  	this.groupCrud.read_Group().subscribe(data => {
- 
-      this.groups = data.map(e => {
-    	let uid = this.user.uid;
-    	let uid_list = e.payload.doc.data()['user_ids']
-    	if (!uid_list){
-    		uid_list = {}
-    	}
-    	console.log("uid", uid)
-        return {
-          id: e.payload.doc.id,
-          Name: e.payload.doc.data()['name'],
-          Owner: e.payload.doc.data()['owner'],
-          Subs: uid_list,
-          Org: "dummyOrg"
-        };
-      })
-    });
-    console.log(this.groups);
-
-  }
-
+	deleteGroup(group) {
+	  	this.groupCrud.delete_Group(group.id)
+	}
 }
+
 
 
