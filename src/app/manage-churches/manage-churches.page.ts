a9547c3383c158b1d3741b402e74f92a1a4a2a43
robwad <darleywaddilove@college.harvard.edu>
2019-12-10 18:23:25 -0800

    check

--- src/app/manage-churches/manage-churches.page.ts
+++ src/app/manage-churches/manage-churches.page.ts
@@ -12,6 +12,8 @@ export class ManageChurchesPage implements OnInit {
 	groups: any;
 	user: any;
 	display_groups: any;
+    selection: any;
+    message: any;
 
 	constructor(private groupCrud: GroupCrudService,
         private firestore: AngularFirestore) { }
@@ -21,6 +23,10 @@ export class ManageChurchesPage implements OnInit {
   		firebase.auth().onAuthStateChanged( user => {
 	  		if (user) { this.user = user }
 	  	});
+
+        // initialize selection
+        this.selection = {};
+
 
         // display the groups owned by current user
 	  	this.groupCrud.read_Group().subscribe(data => { 
@@ -35,7 +41,8 @@ export class ManageChurchesPage implements OnInit {
 			        Name: e.payload.doc.data()['name'],
 			        Owner: e.payload.doc.data()['owner'],
 			        Subs: uid_list,
-			        Org: "dummyOrg"
+			        Org: "dummyOrg",
+                    isOn: false
 			    };
 			})
 			this.display_groups = []
@@ -45,7 +52,37 @@ export class ManageChurchesPage implements OnInit {
 				}
 			}
 		});
+
 	}
+
+    // update selection, a set of the selected groups
+    UpdateSelection(group) {
+        if (group.isOn) {
+            // insert current_uid in previous userIDS
+            this.selection[group.Name] = "dummy"
+        }
+        else {
+            // remove current_uid from previous userIDS
+            delete this.selection[group.Name]
+        }
+        console.log("this.selection", this.selection)
+    }
+
+    // get the subs from each selected group, and the message from the textbox
+    buildAddressList(){
+        console.log("trig")
+        if (this.selection){
+            console.log("trig2")
+            Object.keys(this.selection).forEach(name => {
+                for (let group of this.display_groups){
+                    if (name == group.Name){
+                        console.log("groupName, groupSubs", name, group.Subs)
+                        console.log("message", this.message)
+                    }
+                }
+            })
+        }
+    }    
 
     // remove the group from the groups db
 	deleteGroup(group) {
